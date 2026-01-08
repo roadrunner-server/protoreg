2267f22d1ca7c4495d991bcebd9dcea8d38e6d4e
Rauan Mayemir <rauan@mayemir.io>
2026-01-08 17:38:11 +0500

    Add registry extension custom options test

--- tests/protoreg_plugin_test.go
+++ tests/protoreg_plugin_test.go
@@ -11,6 +11,8 @@ import (
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"go.uber.org/zap"
+	"google.golang.org/genproto/googleapis/api/annotations"
+	"google.golang.org/protobuf/proto"
 )
 
 func TestProtoregInit(t *testing.T) {
@@ -47,6 +49,45 @@ func TestProtoregInit(t *testing.T) {
 
 	unknown, err := registry.FindMethodByFullPath("service.v1.Test/Unknown")
 	assert.Nil(t, unknown)
+}
+
+func TestProtoregInitExtensions(t *testing.T) {
+	cont := endure.New(slog.LevelDebug)
+
+	cfg := &config.Plugin{
+		Version: "2023.3.0",
+		Path:    "configs/.rr-protoreg-init-extensions.yaml",
+	}
+
+	l, oLogger := mocklogger.ZapTestLogger(zap.DebugLevel)
+
+	plugin := &protoreg.Plugin{}
+
+	err := cont.RegisterAll(
+		cfg,
+		l,
+		plugin,
+	)
+	assert.NoError(t, err)
+
+	err = cont.Init()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	require.Equal(t, 1, oLogger.FilterMessageSnippet("protoreg initialized").Len())
+
+	registry := plugin.ProtoRegistry()
+	assert.NotNil(t, registry)
+
+	method, err := registry.FindMethodByFullPath("endpoint.v1.DocumentService/GetDocument")
+	assert.NoError(t, err)
+	assert.NotNil(t, method)
+
+	opts := method.UnwrapMethod().Options()
+	rule, ok := proto.GetExtension(opts, annotations.E_Http).(*annotations.HttpRule)
+	assert.True(t, ok)
+	assert.NotNil(t, rule)
 }
 
 func TestProtoregInitDuplicate(t *testing.T) {
